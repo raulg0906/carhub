f9b6a59b316d5a5cf5a5ab88d2e150090d1255b8
raulg0906 <48417672+raulg0906@users.noreply.github.com>
2023-06-12 19:18:30 -0700

    first commit still a WIP, currently working on searchBar

---
+++ components/index.ts
@@ -0,0 +1,21 @@
+import Hero from './Hero';
+import CustomButton from './CustomButton';
+import Footer from './Footer';
+import Navbar from './Navbar';
+import CustomFilter from './CustomFilter';
+import SearchBar from './SearchBar';
+import SearchManufacturer from './SearchManufacturer';
+
+
+
+
+
+export{
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer
+}
